d885a97d1a738b765049d5cf12ab6f11c4655c5c
Justin Force <justin.force@gmail.com>
2019-09-26 17:18:54 -0700

    Initialize Material-UI with icons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,28 @@
-import React from 'react'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import AccessTimeIcon from '@material-ui/icons/AccessTime'
+import React, { useState } from 'react'
 
-const App = () => <h1>tok</h1>
+const useStyles = makeStyles(theme => ({
+  accessTimeIcon: {
+    marginRight: theme.spacing(1)
+  }
+}))
+
+const App = () => {
+  const classes = useStyles()
+  const [color, setColor] = useState('primary')
+  const toggleColor = () => {
+    const newColor = color === 'primary' ? 'secondary' : 'primary'
+    setColor(newColor)
+  }
+  return (
+    <>
+      <Button variant="contained" color={color} onClick={toggleColor}>
+        <AccessTimeIcon className={classes.accessTimeIcon} />
+        tok
+      </Button>
+    </>
+  )
+}
 export default App
