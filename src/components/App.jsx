1e3f0bf98e76b238365021a4f8e26ad8cf040a40
Justin Force <justin.force@gmail.com>
2019-09-30 09:40:45 -0700

    [wip] Building interface with Material-UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
 import Button from '@material-ui/core/Button'
-import { makeStyles } from '@material-ui/core/styles'
+import {
+  makeStyles,
+  MuiThemeProvider,
+  createMuiTheme
+} from '@material-ui/core/styles'
 import AccessTimeIcon from '@material-ui/icons/AccessTime'
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
+import { lightGreen, deepOrange } from '@material-ui/core/colors'
 
-const useStyles = makeStyles(theme => ({
+const theme = createMuiTheme({
+  palette: { primary: lightGreen, secondary: deepOrange }
+})
+
+const useStyles = makeStyles(() => ({
   accessTimeIcon: {
     marginRight: theme.spacing(1)
   }
@@ -11,18 +20,28 @@ const useStyles = makeStyles(theme => ({
 
 const App = () => {
   const classes = useStyles()
-  const [color, setColor] = useState('primary')
-  const toggleColor = () => {
-    const newColor = color === 'primary' ? 'secondary' : 'primary'
-    setColor(newColor)
-  }
+  const timerDisplayValue = '00:00'
+  const [timerRunning, setTimerRunning] = useState(false)
+  const [lastStart, setLastStart] = useState()
+  const startInterval = useCallback(() => {})
+  const startBreak = useCallback()
+  const pause = useCallback()
   return (
-    <>
-      <Button variant="contained" color={color} onClick={toggleColor}>
+    <MuiThemeProvider theme={theme}>
+      <p>{timerDisplayValue}</p>
+      <Button variant="contained" color="primary" onClick={startInterval}>
         <AccessTimeIcon className={classes.accessTimeIcon} />
-        tok
+        Start Interval
       </Button>
-    </>
+      <Button variant="contained" color="secondary" onClick={startBreak}>
+        <AccessTimeIcon className={classes.accessTimeIcon} />
+        Start Break
+      </Button>
+      <Button variant="contained" onClick={pause}>
+        <AccessTimeIcon className={classes.accessTimeIcon} />
+        Pause
+      </Button>
+    </MuiThemeProvider>
   )
 }
 export default App
